c804300c8c7873e680c3b1eecbbd7bbe382a26c6
Luke Walker <lukeryanwalker2000@gmail.com>
2024-03-02 18:34:16 -0800

    Create 27_remove_element.py

---
+++ 27_remove_element.py
@@ -0,0 +1,15 @@
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+
+    def removeElement(self, nums: List[int], val: int) -> int:
+        i = 0
+        
+        while i < len(nums):
+            if nums[i] == val:
+                del nums[i]
+            else:
+                i += 1
+        
+        return len(nums)
