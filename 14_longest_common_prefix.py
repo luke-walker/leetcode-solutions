958fac3c3ef2cf63d0a872dd6275fbe61cadcc79
Luke Walker <lukeryanwalker2000@gmail.com>
2024-03-06 01:13:33 -0800

    Update 14_longest_common_prefix.py
    Implemented the trie solution

--- 14_longest_common_prefix.py
+++ 14_longest_common_prefix.py
@@ -9,10 +9,42 @@ class Solution:
         for i in range(len(strs[0])):
             c = strs[0][i]
 
-            for j in range(1,len(strs)):
+            for j in range(1, len(strs)):
                 if len(strs[j]) <= i or strs[j][i] != c:
                     return ans
             
             ans += c
 
         return ans
+
+"""
+
+# Time Complexity: O(m*n)
+# Time Complexity: O(m*n)
+# m = # of strings, n = length of shortest string
+
+class Solution:
+
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        trie = {}
+
+        res = len(strs[0])
+        count = res
+
+        for s in strs:
+            curr = trie
+
+            for c in s:
+                if c not in curr.keys():
+                    curr[c] = {}
+                else:
+                    count += 1
+
+                curr = curr[c]
+
+            res = min(res, count)
+            count = 0
+
+        return strs[0][:res]
+
+"""
