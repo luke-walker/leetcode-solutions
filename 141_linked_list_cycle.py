5c1af3e7c3838fea178e164a171e275859794af9
Luke Walker <lukeryanwalker2000@gmail.com>
2024-03-06 00:48:17 -0800

    Create 141_linked_list_cycle.py

---
+++ 141_linked_list_cycle.py
@@ -0,0 +1,23 @@
+# Time Complexity: O(n)
+# Auxiliary Space: O(n)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        cache = set()
+
+        while head:
+            if head in cache:
+                return True
+            
+            cache.add(head)
+
+            head = head.next
+
+        return False
