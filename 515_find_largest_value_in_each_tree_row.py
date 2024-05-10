6b8705e1f1b06151149e661af2692e3da2044051
Luke Walker <lukeryanwalker2000@gmail.com>
2024-05-09 17:42:59 -0700

    Create 515_find_largest_value_in_each_tree_row.py

---
+++ 515_find_largest_value_in_each_tree_row.py
@@ -0,0 +1,30 @@
+# Time Complexity: O(m)
+# Auxiliary Space: O(n)
+# m = # of nodes, n = depth of tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+
+    def largestValues(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def dfs(node, depth):
+            if not node:
+                return
+
+            if len(res) <= depth:
+                res.append(node.val)
+            else:
+                res[depth] = max(res[depth], node.val)
+
+            dfs(node.left, depth+1)
+            dfs(node.right, depth+1)
+
+        dfs(root, 0)
+
+        return res
