309d207cf4794c98ba78af467b2ac83780de0cfa
Luke Walker <lukeryanwalker2000@gmail.com>
2024-05-23 14:13:09 -0700

    Create 412_fizz_buzz.py

---
+++ 412_fizz_buzz.py
@@ -0,0 +1,18 @@
+class Solution:
+
+    def fizzBuzz(self, n: int) -> List[str]:
+        answer = []
+
+        for i in range(1, n + 1):
+            s = ""
+
+            if i % 3 == 0:
+                s += "Fizz"
+            if i % 5 == 0:
+                s += "Buzz"
+            if s == "":
+                s = str(i)
+            
+            answer.append(s)
+        
+        return answer
