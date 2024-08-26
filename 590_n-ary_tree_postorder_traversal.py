02cf26f2bf684c4ebe595b1b7991b4fda8cc952c
Luke Walker <lukeryanwalker2000@gmail.com>
2024-08-26 00:15:03 -0700

    https://leetcode.com/problems/n-ary-tree-postorder-traversal/

---
+++ 590_n-ary_tree_postorder_traversal.py
@@ -0,0 +1,27 @@
+# Time Complexity: O(n)
+# Auxiliary Space: O(n)
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+
+        result = []
+        stack = [root]
+
+        while len(stack) > 0:
+            node = stack.pop()
+
+            result.append(node.val)
+            if node.children is not None:
+                stack.extend(node.children)
+
+        return result[::-1]
