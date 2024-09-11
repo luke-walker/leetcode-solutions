dd3b3f2a28c49d3a0a3b022e6629310cf2e63b1a
Luke Walker <lukeryanwalker2000@gmail.com>
2024-09-10 20:31:18 -0700

    XOR one-liner

--- 2220_minimum_bit_flips_to_convert_number.py
+++ 2220_minimum_bit_flips_to_convert_number.py
@@ -1,12 +1,6 @@
-# Time Complexity: O(n)
+# Time Complexity: O(log(n))
 # Auxiliary Space: O(1)
-# n = max # bits
 
 class Solution:
     def minBitFlips(self, start: int, goal: int) -> int:
-        result = 0
-        for i in range(max(start.bit_length(), goal.bit_length())):
-            if (start >> i) & 1 != (goal >> i) & 1:
-                result += 1
-
-        return result
+        return bin(start^goal).count("1")
