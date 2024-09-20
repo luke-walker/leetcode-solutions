e4abef4539f98df8465d8777ab7193613f0d7791
Luke Walker <lukeryanwalker2000@gmail.com>
2024-09-19 23:20:10 -0700

    https://leetcode.com/problems/shortest-palindrome/

---
+++ 214_shortest_palindrome.py
@@ -0,0 +1,12 @@
+# Time Complexity: O(n)
+# Auxiliary Space: O(n)
+
+class Solution:
+    def shortestPalindrome(self, s: str) -> str:
+        n = len(s)
+        rev = s[::-1]
+        for i in range(n):
+            if s[:n-i] == rev[i:]:
+                return rev[:i] + s
+
+        return ""
