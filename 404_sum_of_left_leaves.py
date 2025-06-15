f05229eeae536a8f78d28c21b1cd43283273e718
Luke Walker <lukeryanwalker2000@gmail.com>
2025-06-14 17:56:13 -0700

    https://leetcode.com/problems/sum-of-left-leaves/

---
+++ 404_sum_of_left_leaves.py
@@ -0,0 +1,20 @@
+# Time Complexity: O(n)
+# Auxiliary Space: O(log(n))
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        total = self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)
+
+        if root.left and not root.left.left and not root.left.right:
+            total += root.left.val
+
+        return total
